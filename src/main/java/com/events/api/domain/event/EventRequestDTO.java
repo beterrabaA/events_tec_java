4ee48e91cac926846726107f7aa53b13344721a9
Beterraba <ls6182315@gmail.com>
2024-08-02 09:17:34 -0300

    create event dto

---
+++ src/main/java/com/events/api/domain/event/EventRequestDTO.java
@@ -0,0 +1,7 @@
+package com.events.api.domain.event;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public record EventRequestDTO(String title, String description, Long date, String city, String uf, boolean remote, String eventUrl,
+                              MultipartFile image) {
+}
